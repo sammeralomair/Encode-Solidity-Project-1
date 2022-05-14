a0ca283c1fcad76f15dc53be8e636cb5c6e1d574
Sammer Alomair <sammeralomair@Sammers-MBP.lan>
2022-05-14 16:49:57 -0700

    committing 2

--- Project/scripts/Ballot/createWallet.ts
+++ Project/scripts/Ballot/createWallet.ts
@@ -1,11 +1,14 @@
 import { ethers } from "ethers";
 import "dotenv/config";
 import * as ballotJson from "../../artifacts/contracts/Ballot.sol/Ballot.json";
+import { SigningKey } from "ethers/lib/utils";
 
 
 async function main() {
     const newWallet = ethers.Wallet.createRandom();
     console.log(newWallet);
+    console.log(newWallet.mnemonic);
+    console.log(newWallet.privateKey);
 }
 
 main().catch((error) => {
