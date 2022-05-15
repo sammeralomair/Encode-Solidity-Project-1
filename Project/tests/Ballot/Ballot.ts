82c82e84bc0bcc5cc41e2547079bb3513143757b
Sammer Alomair <sammeralomair@Sammers-MBP.lan>
2022-05-15 15:50:34 -0700

    need to review ballot test

--- Project/tests/Ballot/Ballot.ts
+++ Project/tests/Ballot/Ballot.ts
@@ -87,15 +87,55 @@ describe("Ballot", function () {
 
   describe("when the voter interact with the vote function in the contract", function () {
     // TODO
-    it("is not implemented", async function () {
-      throw new Error("Not implemented");
+    it("proposal vote count increments by voter's vote weight on successful vote", async function () {
+      const voterAddress = accounts[1].address;
+      const voter = await ballotContract.voters(voterAddress);
+      await giveRightToVote(ballotContract, voterAddress);
+      const voterWeight = voter.weight.toNumber()
+      await ballotContract.connect(accounts[1]).vote(0);
+      expect(ballotContract.proposals[0].voteCount.toNumber()).to.eq(voterWeight);
+    });
+
+    it ("If the voter has voted, the vote function will fail", async function () {
+      const voterAddress = accounts[1].address;
+      await giveRightToVote(ballotContract, voterAddress);
+      await ballotContract.connect(accounts[1]).vote(0);
+      await expect(
+        giveRightToVote(ballotContract, voterAddress)
+      ).to.be.revertedWith("The voter already voted.");
     });
   });
 
   describe("when the voter interact with the delegate function in the contract", function () {
     // TODO
-    it("is not implemented", async function () {
-      throw new Error("Not implemented");
+
+    it ("If delegate already voted, don't increment weight but increment vote count directly", async function () {
+      const voterAddress = accounts[1].address;
+      const delegateAddress = accounts[2].address;
+      await giveRightToVote(ballotContract, voterAddress);
+      await giveRightToVote(ballotContract, delegateAddress);
+      // delegate votes
+      await ballotContract.connect(accounts[2]).vote(0);
+      // delgate is delegated
+      await ballotContract.connect(accounts[1]).delegate(delegateAddress);
+      const delegate = await ballotContract.voters(delegateAddress); 
+      const voter = await ballotContract.voters(voterAddress); 
+      // delegate weight still 0 but vote count increases
+      await expect(delegate.weight.toNumber()).to.eq(0);
+      await expect(voter.weight.toNumber()).to.eq(0);
+      await expect(ballotContract.proposals[0].voteCount.toNumber()).to.eq(2);
+    });
+
+    it("if delegate hasn't voted yet", async function () {
+      const voterAddress = accounts[1].address;
+      const delegateAddress = accounts[2].address;
+      await giveRightToVote(ballotContract, voterAddress);
+      await giveRightToVote(ballotContract, delegateAddress);
+      const delegate = await ballotContract.voters(delegateAddress); 
+      const voter = await ballotContract.voters(voterAddress); 
+      await ballotContract.connect(accounts[1]).delegate(delegateAddress);
+      expect(delegate.weight.toNumber()).to.eq(2);
+      expect(voter.weight.toNumber()).to.eq(0);
     });
   });
 
@@ -122,36 +162,60 @@ describe("Ballot", function () {
 
   describe("when someone interact with the winningProposal function before any votes are cast", function () {
     // TODO
-    it("is not implemented", async function () {
-      throw new Error("Not implemented");
+    it("if no votes are cast yet, it'll output the last proposal", async function () {
+      const winningProposal = await ballotContract.connect(accounts[1]).winningProposal();
+      expect(winningProposal).to.eq(ballotContract.proposals[ballotContract.proposals.length - 1])
     });
   });
 
   describe("when someone interact with the winningProposal function after one vote is cast for the first proposal", function () {
     // TODO
-    it("is not implemented", async function () {
-      throw new Error("Not implemented");
+    it("it'll output the first proposal because it has most votes", async function () {
+      const voterAddress = accounts[1].address;
+      await giveRightToVote(ballotContract, voterAddress);
+      await ballotContract.connect(accounts[1]).vote(0);
+      const winningProposal = await ballotContract.connect(accounts[1]).winningProposal();
+      expect(winningProposal).to.eq(ballotContract.proposals[0])
     });
   });
 
   describe("when someone interact with the winnerName function before any votes are cast", function () {
     // TODO
-    it("is not implemented", async function () {
-      throw new Error("Not implemented");
+    it("if no votes are cast yet, it'll output the name of the last proposal", async function () {
+      const winningName = await ballotContract.connect(accounts[1]).winningName().toString();
+      expect(winningName).to.eq(ballotContract.proposals[ballotContract.proposals.length - 1].name.toString())
     });
   });
 
   describe("when someone interact with the winnerName function after one vote is cast for the first proposal", function () {
     // TODO
-    it("is not implemented", async function () {
-      throw new Error("Not implemented");
+    it("it'll output the name of the first proposal", async function () {
+      const voterAddress = accounts[1].address;
+      await giveRightToVote(ballotContract, voterAddress);
+      await ballotContract.connect(accounts[1]).vote(0);
+      const winningName = await ballotContract.connect(accounts[1]).winningName().toString();
+      expect(winningName).to.eq(ballotContract.proposals[0].name.toString())
     });
   });
 
   describe("when someone interact with the winningProposal function and winnerName after 5 random votes are cast for the proposals", function () {
     // TODO
-    it("is not implemented", async function () {
-      throw new Error("Not implemented");
+    it("will output proposal with highest votes, or the latest if there's a tie", async function () {
+      const voterAddress = accounts[1].address;
+      await giveRightToVote(ballotContract, voterAddress);
+      // do random number picker from proposal indices 5 times and vote each time 
+      await ballotContract.connect(accounts[1]).vote(0);
+      const winningProposal = await ballotContract.connect(accounts[1]).winningProposal();
+      expect(winningProposal).to.eq(ballotContract.proposals[0])
+    });
+
+    it("will output the proposal name with highest votes, or the latest if there's a tie", async function () {
+      const voterAddress = accounts[1].address;
+      await giveRightToVote(ballotContract, voterAddress);
+      // do random number picker from proposal indices 5 times and vote each time 
+      await ballotContract.connect(accounts[1]).vote(0);
+      const winningName = await ballotContract.connect(accounts[1]).winningName().toString();
+      expect(winningName).to.eq(ballotContract.proposals[0].name.toString())
     });
   });
 });
